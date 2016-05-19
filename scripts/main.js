0c545fb77dd7b055771ad2e769af6df9e77bf213
Louis Huang <0563louishuang@gmail.com>
2016-05-18 19:29:45 -0700

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+// Created by louishuang on 2/19/16.
+
+
+
+// scrollspy function
+
+$(document).ready(function () {
+  $('body').scrollspy({target:'#navbar-example'});
+});
+
+
+// tooltip
+
+
+$('.myToolTip').tooltip();
